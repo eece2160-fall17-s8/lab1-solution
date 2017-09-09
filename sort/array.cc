bcf0e48eb46eb51b19125c965d89622b62085ca3
Yifan Sun <sunyifan112358@gmail.com>
2017-09-08 22:38:58 -0400

    sort

--- sort/array.cc
+++ sort/array.cc
@@ -1,12 +1,42 @@
+#include <iostream>
+#include <climits>
+
 void PrintArray(int v[], int size) {
+  for (int i = 0; i < size; i++) {
+    std::cout << v[i] << std::endl;
+  }
 }
 
 void InputArray(int v[], int size) {
+  for (int i = 0; i < size; i++) {
+    std::cin >> v[i];
+  }
 }
 
 void SortArray(int v[], int size) {
+  for (int i = 0; i < size; i++) {
+  
+    // Find min and min_pos
+    int min = INT_MAX;
+    int min_pos = 0;
+    for (int j = i; j < size; j++) {
+      if (v[j] < min) {
+        min = v[j];
+        min_pos = j;
+      }
+    }
+
+    // Swap the i-th with the min
+    int temp = v[i];
+    v[i] = v[min_pos];
+    v[min_pos] = temp;
+  }
 }
 
 int main() {
-  return -1;
+  int v[10];
+  InputArray(v, 10);
+  SortArray(v, 10);
+  PrintArray(v, 10);
+  return 0;
 }
